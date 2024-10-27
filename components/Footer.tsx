1cac971e1907c4c60109bf5d58c3dfa8e6249502
makkentoshi <pochta.zdraste@mail.ru>
2024-10-27 12:50:01 +0500

    footer fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,34 +5,34 @@ const Footer = () => {
   return (
     <footer className="border-t  py-[2rem]">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
-        <div className="text-slate-700 text-lg font-mono">
+        <div className="text-slate-400 text-lg font-mono">
           © 2024 Demographfy
         </div>
 
      
         <div className="flex space-x-6 mt-4 md:mt-0">
           <Link
-            href="https://www.instagram.com/trendwaave"
+            href="https://www.instagram.com/makkentosh1"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-slate-700 hover:text-slate-900"
+            className="text-slate-400 hover:text-slate-900"
           >
             <FaInstagram size={24} />
           </Link>
 
           <Link
-            href="https://wa.me/1234567890"
+            href="https://wa.me/+77007369302"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-slate-700 hover:text-slate-900"
+            className="text-slate-400 hover:text-slate-900"
           >
             <FaWhatsapp size={24} />
           </Link>
         </div>
 
         {/* Contact Information */}
-        <div className="mt-4 md:mt-0 text-slate-700 text-sm font-mono">
-          <p>WhatsApp: +123 456 7890</p>
+        <div className="mt-4 md:mt-0 text-slate-400 text-sm font-mono">
+          <p>WhatsApp: +7 700 736 9302</p>
         </div>
       </div>
     </footer>
